791f8e34d48baa9bbc4e43ea4ed1d929eccda871
David Zhang <straivers98@gmail.com>
2021-07-30 22:44:38 -0400

    Move sandbox into its own subproject

--- scripts/doc.bat
+++ scripts/doc.bat
@@ -1,2 +1,2 @@
 :: Generates documents for only the modules in the file
-cargo doc -p pal -p renderer -p utils -p windowing -p maple --no-deps --open
+cargo doc -p pal -p renderer -p utils -p windowing -p sandbox --no-deps --open
