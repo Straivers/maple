08d7dedf8ebaae42aa5b30be0bdd1d5f5320a80b
David Zhang <straivers98@gmail.com>
2021-07-31 02:13:20 -0400

    Add comment to lint.bat explaining missing win32 crate

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -1,3 +1,5 @@
 @echo off
+:: The win32 crate is ignored because it just passes through windows-rs, which
+:: produces lots of extraneous warnings
 cargo clippy -p renderer -p utils -p windowing -p maple -- --no-deps -W clippy::pedantic
 @echo on
