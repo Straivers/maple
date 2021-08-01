978d0a0c6971c525ba1d5122003a1fae982fe4db
David Zhang <straivers98@gmail.com>
2021-07-31 21:10:48 -0400

    Add win32 manifest accidentally deleted during reorg

---
+++ build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    config_platform();
+}
+
+#[cfg(target_os = "windows")]
+fn config_platform() {
+    embed_resource::compile("resources/app-manifest.rc");
+}
+
+#[cfg(not(target_os = "windows"))]
+fn config_platform() {
+
+}
