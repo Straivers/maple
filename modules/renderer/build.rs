8844775d5c4f931e85c9427506f0c0314bb92e02
David Zhang <straivers98@gmail.com>
2021-08-11 15:09:10 -0400

    Isolate all triangle rendering code into its own module

--- modules/renderer/build.rs
+++ modules/renderer/build.rs
@@ -1,10 +1,32 @@
-use std::fs;
-
-const VERTEX_SHADER_SOURCE: &str = include_str!("shaders/tri.vert");
-const FRAGMENT_SHADER_SOURCE: &str = include_str!("shaders/tri.frag");
+use std::{fs, path::PathBuf};
 
 fn main() {
     println!("cargo:rerun-if-changed=shaders/");
+
+    compile_shaders();
+}
+
+const SHADER_DIR: &str = "shaders/";
+
+fn compile_shaders() {
+    let files = fs::read_dir(SHADER_DIR).unwrap().filter_map(|f| {
+        if let Ok(entry) = f {
+            let is_file = entry.file_type().map_or(false, |f| f.is_file());
+
+            let path = std::path::PathBuf::from(entry.path());
+            let extension = path.extension().unwrap_or_default();
+
+            let is_shader = extension == "vert" || extension == "frag";
+
+            if is_file && is_shader {
+                Some((fs::read(path.as_path()).unwrap(), path))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    });
 
     let mut compiler = shaderc::Compiler::new().unwrap();
     let mut options = shaderc::CompileOptions::new().unwrap();
@@ -12,25 +34,32 @@ fn main() {
     options.set_generate_debug_info();
     options.set_warnings_as_errors();
 
-    let vertex_shader_binary = compiler
-        .compile_into_spirv(
-            VERTEX_SHADER_SOURCE,
-            shaderc::ShaderKind::Vertex,
-            "tri.vert",
-            "main",
-            Some(&options),
-        )
-        .unwrap();
-    fs::write("shaders/tri.vert.spv", &vertex_shader_binary.as_binary_u8()).unwrap();
+    for shader in files {
+        let kind = match shader.1.extension().unwrap().to_str().unwrap() {
+            "vert" => shaderc::ShaderKind::Vertex,
+            "frag" => shaderc::ShaderKind::Fragment,
+            _ => panic!("Unsupported shader type"),
+        };
 
-    let fragment_shader_binary = compiler
-        .compile_into_spirv(
-            FRAGMENT_SHADER_SOURCE,
-            shaderc::ShaderKind::Fragment,
-            "tri.vert",
-            "main",
-            Some(&options),
-        )
-        .unwrap();
-    fs::write("shaders/tri.frag.spv", &fragment_shader_binary.as_binary_u8()).unwrap();
+        let binary = compiler
+            .compile_into_spirv(
+                std::str::from_utf8(&shader.0).unwrap(),
+                kind,
+                shader.1.file_name().unwrap().to_str().unwrap(),
+                "main",
+                Some(&options),
+            )
+            .unwrap();
+
+        let mut dest_name = shader.1.file_stem().unwrap().to_str().unwrap().to_owned();
+        dest_name.push('_');
+        dest_name.push_str(shader.1.extension().unwrap().to_str().unwrap());
+
+        let mut path = PathBuf::new();
+        path.push(SHADER_DIR);
+        path.push(dest_name);
+        path.set_extension("spv");
+
+        fs::write(path, &binary.as_binary_u8()).unwrap();
+    }
 }
--- modules/renderer/src/constants.rs
+++ modules/renderer/src/constants.rs
@@ -1,3 +1,1 @@
 pub const FRAMES_IN_FLIGHT: usize = 2;
-pub const TRIANGLE_VERTEX_SHADER: &[u8] = include_bytes!("../shaders/tri.vert.spv");
-pub const TRIANGLE_FRAGMENT_SHADER: &[u8] = include_bytes!("../shaders/tri.frag.spv");
